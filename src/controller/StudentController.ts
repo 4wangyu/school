30287595b0e7a3389baa8336b2e5e16d9c174a50
4wangyu <4wangyu@gmail.com>
2020-10-23 01:23:57 +0800

    update 500 err msg

--- src/controller/StudentController.ts
+++ src/controller/StudentController.ts
@@ -18,7 +18,9 @@ function suspend(req: Request, res: Response) {
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).json({ message: err.message });
+      res
+        .status(500)
+        .json({ message: 'Internal error, please contact support' });
     });
 }
 
--- src/controller/TeacherStudentController.ts
+++ src/controller/TeacherStudentController.ts
@@ -36,7 +36,9 @@ function register(req: Request, res: Response) {
     .then(() => res.status(204).json())
     .catch((err) => {
       console.error(err);
-      res.status(500).json({ message: err.message });
+      res
+        .status(500)
+        .json({ message: 'Internal error, please contact support' });
     });
 }
 
@@ -57,7 +59,7 @@ async function getCommonStudents(req: Request, res: Response) {
     res.status(200).json({ students: students.map((s) => s.studentEmail) });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: 'Internal error, please contact support' });
   }
 }
 
