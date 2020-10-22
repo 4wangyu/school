bef196b0bb53fc10ac436607f1795a891a0e32aa
4wangyu <4wangyu@gmail.com>
2020-10-22 18:48:44 +0800

    configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    quotes: [2, 'single'],
+    'no-unused-vars': 'off',
+  },
+};
