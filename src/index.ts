fd18f3d01033428439b4fb48d45623aa320557f4
4wangyu <4wangyu@gmail.com>
2020-10-22 20:41:03 +0800

    use express router

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import router from './router';
 
 createConnection()
   .then(async () => {
     const app = express();
     app.use(bodyParser.json());
 
-    app.listen(3000);
+    app.use('/api', router);
 
-    console.log('Express server has started on port 3000.');
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`Server is listening on port: ${port}`));
   })
   .catch(console.error);
